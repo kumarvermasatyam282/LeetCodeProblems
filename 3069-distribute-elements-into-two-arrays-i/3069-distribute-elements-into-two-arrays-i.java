6d742f44ecb59ac44f45a034d852b5af398cceff
Satyam Kumar Verma <kumarvermasatyam282@gmail.com>
2026-08-20 11:49:18 +0530

    Time: 10 ms (7.21%), Space: 46.9 MB (23.8%) - LeetHub

---
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] resultArray(int[] nums) {
+        if(nums.length<=1) return nums;
+        ArrayList<Integer> l1=new ArrayList<>();
+        ArrayList<Integer> l2=new ArrayList<>();
+        l1.add(nums[0]);
+        l2.add(nums[1]);
+        for(int i=2;i<nums.length;i++){ 
+            if(l1.get(l1.size()-1) > l2.get(l2.size()-1)){
+                l1.add(nums[i]);
+            }
+            else l2.add(nums[i]);
+        }
+        l1.addAll(l2);
+
+        System.out.print(l1);
+        System.out.print(l2);
+
+        int[] ans=new int[nums.length];
+        for(int i=0;i<l1.size() ;i++){
+            ans[i]=l1.get(i);
+        }
+        return ans;
+    }
+}
